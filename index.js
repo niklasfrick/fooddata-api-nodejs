8d4d21b7cbce60617d06edcf534e30f0ddf0f3f4
niklasfrick <niklas@Niklas-MBP.fritz.box>
2021-07-01 20:02:43 +0200

    chore: husky pre-commit

--- index.js
+++ index.js
@@ -1,69 +1,68 @@
-const fetch = require("isomorphic-unfetch")
-const querystring = require("querystring")
+const fetch = require('isomorphic-unfetch')
+const querystring = require('querystring')
 
 class FoodDataCentral {
-    constructor(apiKey = "DEMO_KEY") {
-        this.api_key = apiKey
-        this.basePath = "https://api.nal.usda.gov/fdc"
-    } 
+  constructor (apiKey = 'DEMO_KEY') {
+    this.api_key = apiKey
+    this.basePath = 'https://api.nal.usda.gov/fdc'
+  }
 
-    request(endpoint = "", options = {}) {
-        let url = this.basePath + endpoint
+  request (endpoint = '', options = {}) {
+    const url = this.basePath + endpoint
 
-        let headers = {
-            'Content-type': 'application/json'
-        }
-
-        let config = {
-            ...options,
-            ...headers
-        }
-
-        return fetch(url, config).then(r => {
-            if (r.ok) {
-                return r.json()
-            }
-            throw new Error(r.statusText)
-        })
+    const headers = {
+      'Content-type': 'application/json'
     }
 
-    search(search_term, searchOptions = "") {
-        if (search_term === undefined) {
-            throw new Error("Error. Search Term can't be empty. (Retrieve all foods by searching with '*')")
-        }
-
-        let options = {
-            "query": search_term
-        }
-
-        if (searchOptions != undefined) {
-            Object.assign(options, searchOptions)
-        }
-        
-
-        let qs = options ? "&" + querystring.stringify(options) : ""
-
-        let url = "/v1/foods/search?api_key=" + this.api_key + qs
-        let config = {
-            method: 'GET'
-        }
-        return this.request(url, config)
+    const config = {
+      ...options,
+      ...headers
     }
 
-    details(fdcid, detailOptions = "") {
-        if (fdcid === undefined) {
-            throw new Error("Error. FDC ID can't be empty.")
-        } else if (String((Math.abs(fdcid))).length < 6) {
-            throw new Error("Error. Wrong FDC ID format.")
-        }
+    return fetch(url, config).then(r => {
+      if (r.ok) {
+        return r.json()
+      }
+      throw new Error(r.statusText)
+    })
+  }
 
-        let url = "/v1/food/" + fdcid + "?api_key=" + this.api_key
-
-        let config = {
-            method: 'GET'
-        }
-        return this.request(url, config)
+  search (searchTerm, searchOptions = '') {
+    if (searchTerm === undefined) {
+      throw new Error("Error. Search Term can't be empty. (Retrieve all foods by searching with '*')")
     }
+
+    const options = {
+      query: searchTerm
+    }
+
+    if (searchOptions !== undefined) {
+      Object.assign(options, searchOptions)
+    }
+
+    const qs = options ? '&' + querystring.stringify(options) : ''
+
+    const url = '/v1/foods/search?api_key=' + this.api_key + qs
+    const config = {
+      method: 'GET'
+    }
+    return this.request(url, config)
+  }
+
+  details (fdcid, detailOptions = '') {
+    if (fdcid === undefined) {
+      throw new Error("Error. FDC ID can't be empty.")
+    } else if (String((Math.abs(fdcid))).length < 6) {
+      throw new Error('Error. Wrong FDC ID format.')
+    }
+
+    const url = '/v1/food/' + fdcid + '?api_key=' + this.api_key
+
+    const config = {
+      method: 'GET'
+    }
+    return this.request(url, config)
+  }
 }
 
 module.exports = FoodDataCentral
