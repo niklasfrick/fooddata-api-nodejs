aaac3ad3ee6930a711ff0f6baf567038d9b3d27c
niklasfrick <niklas@Niklas-MBP.fritz.box>
2021-07-05 09:24:38 +0200

    :white_check_mark: chore: added local tests

--- test/local-tests.js
+++ test/local-tests.js
@@ -4,9 +4,18 @@ require('dotenv').config()
 
 const API_KEY = process.env.FOODDATACENTRAL_API_KEY
 
+// async function run() {
+//     const client = new FoodDataCentral(API_KEY)
+//     const searchresult1 = await client.list([123456], {format: 2, nutrients: [203, 204]})
+//     console.log(searchresult1)
+// }
+
+
+// run()
+
 async function run() {
     const client = new FoodDataCentral(API_KEY)
-    const searchresult1 = await client.list([123456], {format: 2, nutrients: [203, 204]})
+    const searchresult1 = await client.request("/v1/food/1103063?format=abridged&nutrients=203", {'Content-type': 'application/json'})
     console.log(searchresult1)
 }
 
